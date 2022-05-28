340ab01a422ee6072b852c114329d0a747c00340
kkrsnik <klemen1krsnik@gmail.com>
2022-05-28 20:02:42 +0200

    init

---
+++ src/main/java/si/unilj/fri/KP_Rest/KpRestApplication.java
@@ -0,0 +1,13 @@
+package si.unilj.fri.KP_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/unilj/fri/KP_Rest/KpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package si.unilj.fri.KP_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
